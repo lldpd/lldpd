563e2dfc9648e35868de04b20d97ad198d9f9a35
Vincent Bernat <bernat@luffy.cx>
2009-07-09 21:30:14 +0200

    Compatibility with gcc 2.95.x: add spaces around __VA__ARGS__

--- src/lldpd.h
+++ src/lldpd.h
@@ -416,13 +416,13 @@ char	*dmi_asset();
 /* log.c */
 void             log_init(int);
 void             log_warn(const char *, ...);
-#define LLOG_WARN(x,...) log_warn("%s: " x, __FUNCTION__, ##__VA_ARGS__)
+#define LLOG_WARN(x,...) log_warn("%s: " x, __FUNCTION__ , ## __VA_ARGS__)
 void             log_warnx(const char *, ...);
-#define LLOG_WARNX(x,...) log_warnx("%s: " x,  __FUNCTION__, ##__VA_ARGS__)
+#define LLOG_WARNX(x,...) log_warnx("%s: " x,  __FUNCTION__ , ## __VA_ARGS__)
 void             log_info(const char *, ...);
-#define LLOG_INFO(x,...) log_info("%s: " x, __FUNCTION__, ##__VA_ARGS__)
+#define LLOG_INFO(x,...) log_info("%s: " x, __FUNCTION__ , ## __VA_ARGS__)
 void             log_debug(const char *, ...);
-#define LLOG_DEBUG(x,...) log_debug("%s: " x, __FUNCTION__, ##__VA_ARGS__)
+#define LLOG_DEBUG(x,...) log_debug("%s: " x, __FUNCTION__ , ## __VA_ARGS__)
 void             fatal(const char *);
 void             fatalx(const char *);
 
