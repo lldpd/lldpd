586952454aeeb5f9b87be550386e2614769a2ecd
Vincent Bernat <bernat@luffy.cx>
2009-08-18 14:05:52 +0200

    Fix LSB header in provided Debian init.d script.
    Thanks to a patch from Petter Reinholdtsen:
     http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=542161

--- debian/lldpd.init.d
+++ debian/lldpd.init.d
@@ -1,8 +1,8 @@
 #! /bin/sh
 ### BEGIN INIT INFO
-# Provides:          skeleton
-# Required-Start:    $remote_fs $network
-# Required-Stop:     $network $remote_fs
+# Provides:          lldpd
+# Required-Start:    $remote_fs $network $syslog
+# Required-Stop:     $network $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: LLDP daemon
