d902e68880f1c0aa9b18e686276fba7d5c07edfd
Vincent Bernat <bernat@luffy.cx>
2012-12-20 09:23:01 +0100

    build: make autogen.sh a little bit more generic

--- autogen.sh
+++ autogen.sh
@@ -13,14 +13,16 @@ libtoolize --copy --force
 echo "autogen.sh: reconfigure with autoreconf"
 autoreconf -vif -I m4 || {
     echo "autogen.sh: autoreconf has failed ($?), let's do it manually"
-    for dir in . libevent; do
+    for dir in . *; do
+        [ -d "$dir" ] || continue
+        [ -f "$dir"/configure.ac ] || [ -f "$dir"/configure.in ] || continue
 	echo "autogen.sh: configure `basename \`readlink -f $dir\``"
-	(cd $dir && aclocal -I m4)
-	(cd $dir && libtoolize --automake --copy --force)
-	(cd $dir && aclocal -I m4)
-	(cd $dir && autoconf --force)
-	(cd $dir && autoheader)
-	(cd $dir && automake --add-missing --copy --force-missing)
+	(cd "$dir" && aclocal -I m4)
+	(cd "$dir" && libtoolize --automake --copy --force)
+	(cd "$dir" && aclocal -I m4)
+	(cd "$dir" && autoconf --force)
+	(cd "$dir" && autoheader)
+	(cd "$dir" && automake --add-missing --copy --force-missing)
     done
 }
 
