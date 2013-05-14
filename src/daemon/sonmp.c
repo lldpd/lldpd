0728cb8205f7d3c285f4e8b502b0bb243c419824
Vincent Bernat <bernat@luffy.cx>
2013-05-15 00:10:17 +0200

    sonmp: allow SONMP unittests to work without a global config

--- src/daemon/sonmp.c
+++ src/daemon/sonmp.c
@@ -364,7 +364,8 @@ sonmp_decode(struct lldpd *cfg, char *frame, int s,
 		goto malformed;
 	}
 	TAILQ_INSERT_TAIL(&chassis->c_mgmt, mgmt, m_entries);
-	chassis->c_ttl = cfg->g_config.c_tx_interval * cfg->g_config.c_tx_hold;
+	chassis->c_ttl = cfg?(cfg->g_config.c_tx_interval * cfg->g_config.c_tx_hold):
+	    LLDPD_TTL;
 
 	port->p_id_subtype = LLDP_PORTID_SUBTYPE_LOCAL;
 	if (asprintf(&port->p_id, "%02x-%02x-%02x",
