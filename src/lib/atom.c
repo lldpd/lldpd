lldpd/lldpd

commit 5aeda65433fd41b7bb67f908d8c4741f045ce508

liblldpctl: fix infinite loop when user is reading fixed chunks
ISSUE:

_lldpctl_do_something() has the following loop for receiving a complete
message:

while ((bytes_needed = ctl_msg_recv_unserialized(...)) > 0) {
    _lldpctl_needs(bytes_needed)
}

ctl_msg_recv_unserialized() processes the received message and determines
how many more bytes are needed. First time, it requests "header" worth of
bytes. Once it receives that, it looks at header->len to determine next
set of bytes.

_lldpctl_needs() calls the connection's receive() routine to receive a
chunk of data and copies that over the a buffer in the connection structure,
either by malloc/copy or realloc/memmove. This buffer is called
conn->input_buffer. conn->input_buffer_len stores how many bytes are
currently in conn->input_buffer.

The issue is with the following statement in _lldpctl_needs():

        if (conn->input_buffer_len >= length) return 0;

where length is bytes_needed in the above illustration.

Suppose the message requires 16578 bytes. The connection's receive()
routine provided by the application reads in chunks of 1024 bytes.
With the above logic, the loop will execute successfully for 16 times
with the following resultant state:

conn->input_buffer_len = 16384
bytes_needed = 194

The above conditional statement in the beginning of _lldpctl_needs()
will cause it to return 0. ctl_msg_recv_unserialized() will keep asking
for 194 more bytes. The while() loop goes ad infinitum.

PATCH:

Remove the above statement. It is not needed.

src/lib/connection.c
@@ -157,7 +157,6 @@ _lldpctl_needs(lldpctl_conn_t *conn, size_t length)
 {
 	uint8_t *buffer = NULL;
 	ssize_t  rc;
-	if (conn->input_buffer_len >= length) return 0;
 
 	if ((buffer = malloc(length)) == NULL)
 		return SET_ERROR(conn, LLDPCTL_ERR_NOMEM);

commit bdc954b77ada430ca4897c4adc1b25e2103a4450

lib: fix notification by reading one byte at a time from Unix socket
Internally, several bytes will be read at once and the notification
should be detected earlier. This fixes a regression introduced in
5aeda65433fd41b7bb67f908d8c4741f045ce508 because we relied on datagram
boundary semantics. This was not important when pulling information
but we still used this semantic when pushing information. Closes: #43.

src/lib/atom.c
@@ -369,8 +369,7 @@ lldpctl_watch_callback(lldpctl_conn_t *conn,
 int
 lldpctl_watch(lldpctl_conn_t *conn)
 {
-	int rc;
-	size_t much;
+	int rc = 0;
 
 	RESET_ERROR(conn);
 
@@ -378,10 +377,8 @@ lldpctl_watch(lldpctl_conn_t *conn)
 		return SET_ERROR(conn, LLDPCTL_ERR_INVALID_STATE);
 
 	conn->watch_triggered = 0;
-	much = 512;
 	while (!conn->watch_triggered) {
-		rc = _lldpctl_needs(conn, much);
-		much += 512;
+		rc = _lldpctl_needs(conn, 1);
 		if (rc < 0)
 			return SET_ERROR(conn, rc);
 	}

src/lib/connection.c
@@ -61,7 +61,7 @@ sync_send(lldpctl_conn_t *lldpctl,
 	return nb;
 }
 
-/* Statiscally receive data from remote end. */
+/* Statically receive data from remote end. */
 static ssize_t
 sync_recv(lldpctl_conn_t *lldpctl,
     const uint8_t *data, size_t length, void *user_data)