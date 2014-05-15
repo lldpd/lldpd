98e2e47549963a485fe1435a5b190d7d2059843f
Vincent Bernat <vincent@bernat.im>
2014-05-15 14:46:24 +0200

    travis: misc change to trigger a rebuild

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+# Setup dev environment for Travis
+
 set -e
 
 case "$(uname -s)" in
     Darwin)
+        # OS X
         brew update
         brew install \
             pkg-config autoconf automake libtool \
             readline libevent net-snmp jansson libxml2 check
         ;;
     *)
+        # Linux
         sudo apt-get -qqy update
         sudo apt-get -qqy install \
             automake autoconf libtool pkg-config \
