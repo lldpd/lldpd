0db5b7bbffa689670eb327303b4907748b82fb82
Vincent Bernat <vincent@bernat.im>
2014-05-16 02:03:46 +0200

    travis: don't try to install launchd/systemd stuff
    This won't work as they are not installed inside prefix.

--- .travis/run.sh
+++ .travis/run.sh
@@ -4,4 +4,4 @@ set -e
 
 ./autogen.sh
 ./configure $LLDPD_CONFIG_ARGS
-make distcheck DISTCHECK_CONFIGURE_FLAGS="$LLDPD_CONFIG_ARGS"
+make distcheck DISTCHECK_CONFIGURE_FLAGS="$LLDPD_CONFIG_ARGS --with-systemdsystemunitdir=no --with-launchddaemonsdir=no"
