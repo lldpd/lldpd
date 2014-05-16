78f816e9af24de10745155b245c66093f6b15601
Vincent Bernat <vincent@bernat.im>
2014-05-16 09:54:05 +0200

    travis: when running coverity, skip regular build

--- .travis/run.sh
+++ .travis/run.sh
@@ -4,4 +4,10 @@ set -e
 
 ./autogen.sh
 ./configure $LLDPD_CONFIG_ARGS
-make distcheck DISTCHECK_CONFIGURE_FLAGS="$LLDPD_CONFIG_ARGS --with-systemdsystemunitdir=no --with-launchddaemonsdir=no"
+
+# When running coverity, do not run make
+[ "${COVERITY_SCAN_BRANCH}" != 1 ] || exit 0
+
+LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-systemdsystemunitdir=no"
+LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-launchddaemonsdir=no"
+make distcheck DISTCHECK_CONFIGURE_FLAGS="$LLDPD_CONFIG_ARGS"
