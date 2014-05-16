b2c51a1d62be4be6ace13b60ecc9e0a691ba2d36
Vincent Bernat <vincent@bernat.im>
2014-05-16 10:34:16 +0200

    travis: integrate coverity script in regular run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -5,9 +5,16 @@ set -e
 ./autogen.sh
 ./configure $LLDPD_CONFIG_ARGS
 
-# When running coverity, do not run make
-[ "${COVERITY_SCAN_BRANCH}" != 1 ] || exit 0
-
-LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-systemdsystemunitdir=no"
-LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-launchddaemonsdir=no"
-make distcheck DISTCHECK_CONFIGURE_FLAGS="$LLDPD_CONFIG_ARGS"
+if [ x"${RUN_COVERITY}" = x"1" ]; then
+    # Coverity build
+    [ x"${COVERITY_SCAN_TOKEN}" = x"" ] || \
+        curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh | \
+        COVERITY_SCAN_PROJECT_NAME="$TRAVIS_REPO_SLUG" \
+        COVERITY_SCAN_BUILD_COMMAND="make" \
+        bash
+else
+    # Regular build
+    LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-systemdsystemunitdir=no"
+    LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-launchddaemonsdir=no"
+    make distcheck DISTCHECK_CONFIGURE_FLAGS="$LLDPD_CONFIG_ARGS"
+fi
