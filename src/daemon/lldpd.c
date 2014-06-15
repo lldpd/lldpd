lldpd/lldpd

commit 46baf62775eaca47075e1d9966a2103fd466e4b3

lldpd: add possibility to disable LLDP
LLDP was mandatory. It is now possible to disable it entirely with
`-ll`. In this case, when no neighbor is detected, the first enabled
protocol will be used. For example, with `-ll -c`, lldpd will act as a
pure CDP daemon.

src/daemon/lldpd.c
@@ -711,10 +711,18 @@ lldpd_send_all(struct lldpd *cfg)
 			}
 		}
 
-		if (!sent)
-			/* Nothing was sent for this port, let's speak LLDP */
-			cfg->g_protocols[0].send(cfg,
-			    hardware);
+		if (!sent) {
+			/* Nothing was sent for this port, let's speak the first
+			 * available protocol. */
+			for (i = 0; cfg->g_protocols[i].mode != 0; i++) {
+				if (!cfg->g_protocols[i].enabled) continue;
+				cfg->g_protocols[i].send(cfg,
+				    hardware);
+				break;
+			}
+			if (cfg->g_protocols[i].mode == 0)
+				LLOG_WARNX("no protocol enabled, dunno what to send");
+		}
 	}
 }
 
@@ -1027,6 +1035,7 @@ lldpd_main(int argc, char *argv[])
 			for (i=0; protos[i].mode != 0; i++) {
 				if (ch == protos[i].arg) {
 					protos[i].enabled++;
+					protos[i].enabled %= 3;
 					/* When an argument enable
 					   several protocols, only the
 					   first one can be forced. */

src/daemon/lldpd.8
@@ -111,7 +111,7 @@ even when there is no EDP peer detected.
 Force to send LLDP packets even when there is no LLDP peer detected
 but there is a peer speaking another protocol detected. By default,
 LLDP packets are sent when there is a peer speaking LLDP detected or
-when there is no peer at all.
+when there is no peer at all. If repeated, LLDP is disabled.
 .It Fl r
 Receive-only mode. With this switch,
 .Nm

commit b8a802bc7d8a748018aa7cea139ad5a50eb1adb5

lldpd: fix how LLDP can be disabled
Commit 46baf62775eaca47075e1d9966a2103fd466e4b3 broke the ability to
disable LLDP. Instead of trying to be smart, just enumerate all the
possible cases.

src/daemon/lldpd.c
@@ -1398,11 +1398,8 @@ lldpd_main(int argc, char *argv[], char *envp[])
 			found = 0;
 			for (i=0; protos[i].mode != 0; i++) {
 				if (ch == protos[i].arg) {
-					if (protos[i].enabled < 3) {
-						found = 1;
-						if (protos[i].enabled++ == 1)
-							break;
-					}
+					found = 1;
+					protos[i].enabled++;
 				}
 			}
 			if (!found)
@@ -1577,13 +1574,27 @@ lldpd_main(int argc, char *argv[], char *envp[])
 
 	log_debug("main", "initialize protocols");
 	cfg->g_protocols = protos;
-	for (i=0; protos[i].mode != 0; i++)
+	for (i=0; protos[i].mode != 0; i++) {
+
+		/* With -ll, disable LLDP */
+		if (protos[i].mode == LLDPD_MODE_LLDP)
+			protos[i].enabled %= 3;
+		/* With -ccc force CDPV2, enable CDPV1 */
+		if (protos[i].mode == LLDPD_MODE_CDPV1 && protos[i].enabled == 3) {
+			protos[i].enabled = 1;
+		}
+		/* With -cc force CDPV1, enable CDPV2 */
+		if (protos[i].mode == LLDPD_MODE_CDPV2 && protos[i].enabled == 2) {
+			protos[i].enabled = 1;
+		}
+
 		if (protos[i].enabled > 1)
 			log_info("main", "protocol %s enabled and forced", protos[i].name);
 		else if (protos[i].enabled)
 			log_info("main", "protocol %s enabled", protos[i].name);
 		else
 			log_info("main", "protocol %s disabled", protos[i].name);
+	    }
 
 	TAILQ_INIT(&cfg->g_hardware);
 	TAILQ_INIT(&cfg->g_chassis);