24cce58cfc1bc9065113059ecec7bd1d4bab4428
Vincent Bernat <vincent@bernat.im>
2014-12-03 22:28:03 +0100

    lldpd: really ensure that port checksum can never be 0

--- src/daemon/lldpd.c
+++ src/daemon/lldpd.c
@@ -315,7 +315,7 @@ lldpd_reset_timer(struct lldpd *cfg)
 		/* Port change is detected by computing a checksum. 0 means the
 		 * checksum never was computed (new interface). */
 		cksum  = frame_checksum(output, output_len, 0);
-		cksum &= 1;
+		cksum |= 1;
 		free(output);
 		if (cksum != hardware->h_lport_cksum) {
 			log_debug("localchassis",
