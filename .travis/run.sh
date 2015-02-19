c2af3a9cbd02791a3743feaf3c3d41e8921e6f1d
Vincent Bernat <vincent@bernat.im>
2015-02-19 17:05:18 +0100

    travis: don't configure with CFLAGS=-Werror
    This is unsupported. Instead, call `make CFLAGS=-Werror`.

--- .travis/run.sh
+++ .travis/run.sh
@@ -15,17 +15,18 @@ case "${RUN_COVERITY}","${TRAVIS_BRANCH}" in
 esac
 
 ./autogen.sh
-./configure $LLDPD_CONFIG_ARGS CFLAGS="-O0 -g -Wall -Werror"
+./configure $LLDPD_CONFIG_ARGS CFLAGS="-O0 -g"
 
 if [ x"${RUN_COVERITY}" = x"1" ]; then
     # Coverity build
     [ x"${COVERITY_SCAN_TOKEN}" = x"" ] || \
         curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh | \
         COVERITY_SCAN_PROJECT_NAME="$TRAVIS_REPO_SLUG" \
-        COVERITY_SCAN_BUILD_COMMAND="make" \
+        COVERITY_SCAN_BUILD_COMMAND="make CFLAGS=-Werror" \
         bash
 else
     # Regular build
+    make all check CFLAGS=-Werror
     LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-systemdsystemunitdir=no"
     LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-launchddaemonsdir=no"
     LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-sysusersdir=no"
