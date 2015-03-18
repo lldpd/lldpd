c642d8c7b9e4cd52f3efc155ce402330949c4f57
Vincent Bernat <vincent@bernat.im>
2015-03-18 14:34:54 +0100

    priv: fallback to SOCK_DGRAM if SOCK_SEQPACKET not available

--- src/daemon/priv.c
+++ src/daemon/priv.c
@@ -591,8 +591,16 @@ priv_init(const char *chrootdir, int ctl, uid_t uid, gid_t gid)
 	int pair[2];
 
 	/* Create socket pair */
-	if (socketpair(AF_UNIX, SOCK_SEQPACKET, PF_UNSPEC, pair) < 0)
-		fatal("privsep", "unable to create socket pair for privilege separation");
+	if (socketpair(AF_UNIX, SOCK_SEQPACKET, PF_UNSPEC, pair) < 0) {
+		if (errno == EAFNOSUPPORT ||
+		    errno == EOPNOTSUPP ||
+		    errno == EPROTONOSUPPORT) {
+			if (socketpair(AF_UNIX, SOCK_DGRAM, PF_UNSPEC, pair) < 0) {
+				fatal("privsep",
+				    "unable to create socket pair for privilege separation");
+			}
+		}
+	}
 
 	priv_unprivileged_fd(pair[0]);
 	priv_privileged_fd(pair[1]);
