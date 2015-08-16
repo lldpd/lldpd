8e8d9a270b31b7a174d21357a5d56b22560b62e0
Vincent Bernat <vincent@bernat.im>
2015-08-16 13:57:00 +0200

    build: automatically reuse regular configure args for distcheck

--- .travis/run.sh
+++ .travis/run.sh
@@ -27,8 +27,5 @@ if [ x"${RUN_COVERITY}" = x"1" ]; then
 else
     # Regular build
     make all check CFLAGS=-Werror
-    LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-systemdsystemunitdir=no"
-    LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-launchddaemonsdir=no"
-    LLDPD_CONFIG_ARGS="$LLDPD_CONFIG_ARGS --with-sysusersdir=no"
-    make distcheck DISTCHECK_CONFIGURE_FLAGS="$LLDPD_CONFIG_ARGS"
+    make distcheck
 fi
