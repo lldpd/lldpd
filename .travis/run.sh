b037b93d3a6b0a5d545d4e9a47799e231e5875da
Vincent Bernat <vincent@bernat.im>
2015-12-13 09:49:53 +0100

    travis: enable PIE

--- .travis/run.sh
+++ .travis/run.sh
@@ -15,7 +15,7 @@ case "${RUN_COVERITY}","${TRAVIS_BRANCH}" in
 esac
 
 ./autogen.sh
-./configure $LLDPD_CONFIG_ARGS CFLAGS="-O0 -g"
+./configure $LLDPD_CONFIG_ARGS --enable-pie CFLAGS="-O0 -g"
 
 if [ x"${RUN_COVERITY}" = x"1" ]; then
     # Coverity build
