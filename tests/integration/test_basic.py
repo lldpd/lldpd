lldpd/lldpd

commit a403df6662cd506fbf7fee8a46ebbf209142ffd7

interfaces: fix alias handling
And try to add some simple tests for them. The manual page says that if
an alias is provided, the default is to use the alias as description and
port name as port ID. We try to respect that.

The tests are not as complete as they could be because the MSAP changes
due to the port ID change and this makes tests more complex.

src/daemon/interfaces.c
@@ -499,43 +499,44 @@ interfaces_helper_port_name_desc(struct lldpd *cfg,
 {
 	struct lldpd_port *port = &hardware->h_lport;
 
-	if (port->p_id_subtype == LLDP_PORTID_SUBTYPE_LOCAL)
-		return;
-
 	/* We need to set the portid to what the client configured.
 	   This can be done from the CLI.
 	*/
-	switch (cfg->g_config.c_lldp_portid_type) {
-	case LLDP_PORTID_SUBTYPE_IFNAME:
-		log_debug("interfaces", "use ifname for %s",
-			  hardware->h_ifname);
-		port->p_id_subtype = LLDP_PORTID_SUBTYPE_IFNAME;
-		port->p_id_len = strlen(hardware->h_ifname);
-		free(port->p_id);
-		if ((port->p_id = calloc(1, port->p_id_len)) == NULL)
-			fatal("interfaces", NULL);
-		memcpy(port->p_id, hardware->h_ifname, port->p_id_len);
-		break;
-	case LLDP_PORTID_SUBTYPE_LLADDR:
-		/* fall through so we use the mac address */
-	default:
-		log_debug("interfaces", "use MAC address for %s",
-			  hardware->h_ifname);
-		port->p_id_subtype = LLDP_PORTID_SUBTYPE_LLADDR;
-		free(port->p_id);
-		if ((port->p_id = calloc(1, ETHER_ADDR_LEN)) == NULL)
-			fatal("interfaces", NULL);
-		memcpy(port->p_id, hardware->h_lladdr, ETHER_ADDR_LEN);
-		port->p_id_len = ETHER_ADDR_LEN;
-	}
+	int has_alias = (iface->alias != NULL && strlen(iface->alias) != 0);
+	int portid_type = cfg->g_config.c_lldp_portid_type;
+	if (portid_type == LLDP_PORTID_SUBTYPE_IFNAME ||
+	    (portid_type == LLDP_PORTID_SUBTYPE_UNKNOWN && has_alias) ||
+	    (portid_type == LLDP_PORTID_SUBTYPE_LOCAL && has_alias)) {
+		if (portid_type != LLDP_PORTID_SUBTYPE_LOCAL) {
+			log_debug("interfaces", "use ifname for %s",
+			    hardware->h_ifname);
+			port->p_id_subtype = LLDP_PORTID_SUBTYPE_IFNAME;
+			port->p_id_len = strlen(hardware->h_ifname);
+			free(port->p_id);
+			if ((port->p_id = calloc(1, port->p_id_len)) == NULL)
+				fatal("interfaces", NULL);
+			memcpy(port->p_id, hardware->h_ifname, port->p_id_len);
+		}
 
-	if (iface->alias != NULL && strlen(iface->alias) != 0) {
 		/* use the actual alias in the port description */
 		log_debug("interfaces", "using alias in description for %s",
 			  hardware->h_ifname);
 		free(port->p_descr);
-		port->p_descr = strdup(iface->alias);
+		if (has_alias) {
+			port->p_descr = strdup(iface->alias);
+		}
 	} else {
+		if (portid_type != LLDP_PORTID_SUBTYPE_LOCAL) {
+			log_debug("interfaces", "use MAC address for %s",
+			    hardware->h_ifname);
+			port->p_id_subtype = LLDP_PORTID_SUBTYPE_LLADDR;
+			free(port->p_id);
+			if ((port->p_id = calloc(1, ETHER_ADDR_LEN)) == NULL)
+				fatal("interfaces", NULL);
+			memcpy(port->p_id, hardware->h_lladdr, ETHER_ADDR_LEN);
+			port->p_id_len = ETHER_ADDR_LEN;
+		}
+
 		/* use the ifname in the port description until alias is set */
 		log_debug("interfaces", "using ifname in description for %s",
 			  hardware->h_ifname);

src/client/conf-lldp.c
@@ -426,7 +426,7 @@ register_commands_configure_lldp(struct cmd_node *configure,
 		NULL, NULL, NULL);
 
 	for (lldpctl_map_t *b_map =
-		     lldpctl_key_get_map(lldpctl_k_config_lldp_portid_type);
+		 lldpctl_key_get_map(lldpctl_k_config_lldp_portid_type);
 	     b_map->string; b_map++) {
 		if (!strcmp(b_map->string, "ifname")) {
 			commands_new(

tests/R1.expected
@@ -76,8 +76,8 @@ Interface:    iface2, via: LLDP, RID: 1, Time: 0 day
     Capability:   Wlan, off
     Capability:   Station, on
   Port:        
-    PortID:       mac 50:54:0e:ec:6e:77
-    PortDescr:    iface2
+    PortID:       ifname iface2
+    PortDescr:    SecondInterface
   LLDP-MED:    
     Device Type:  Media Endpoint (Class II)
     Capability:   Capabilities
@@ -199,8 +199,8 @@ Interface:    iface2, via: LLDP, RID: 1, Time: 0 day
     Capability:   Wlan, off
     Capability:   Station, on
   Port:        
-    PortID:       mac 50:54:0e:ec:6e:77
-    PortDescr:    iface2
+    PortID:       ifname iface2
+    PortDescr:    SecondInterface
   VLAN:         450 iface2.450
   VLAN:         451 iface2.451
   VLAN:         452 iface2.452
@@ -236,8 +236,8 @@ Interface:    iface2, via: LLDP, RID: 1, Time: 0 day
     Capability:   Wlan, off
     Capability:   Station, on
   Port:        
-    PortID:       mac 50:54:0e:ec:6e:77
-    PortDescr:    iface2
+    PortID:       ifname iface2
+    PortDescr:    SecondInterface
   VLAN:         450 iface2.450
   VLAN:         452 iface2.452
   LLDP-MED:    
@@ -415,8 +415,8 @@ Interface:    iface2, via: LLDP, RID: 1, Time: 0 day
     Capability:   Wlan, off
     Capability:   Station, off
   Port:        
-    PortID:       mac 50:54:0e:ec:6e:77
-    PortDescr:    iface2
+    PortID:       ifname iface2
+    PortDescr:    SecondInterface
     MDI Power:    supported: yes, enabled: yes, pair control: yes
       Device type:  PSE
       Power pairs:  spare
@@ -463,8 +463,8 @@ Interface:    iface2, via: LLDP, RID: 1, Time: 0 day
     Capability:   Wlan, off
     Capability:   Station, off
   Port:        
-    PortID:       mac 50:54:0e:ec:6e:77
-    PortDescr:    iface2
+    PortID:       ifname iface2
+    PortDescr:    SecondInterface
     MDI Power:    supported: yes, enabled: yes, pair control: yes
       Device type:  PSE
       Power pairs:  spare

tests/integration-tests.in
@@ -272,6 +272,8 @@ process_commands() {
 }
 
 start_tests() {
+    # Set the environment
+    run R2 ip link set dev iface2 alias "SecondInterface"
     # Start lldpd on each VM
     run R1 ./libtool execute src/daemon/lldpd -M 1 -L \$PWD/src/client/lldpcli
     run R2 ./libtool execute src/daemon/lldpd -M 2 -L \$PWD/src/client/lldpcli

commit e1717397eb15c1b378093891aa8f9381ff440939

interfaces: fix setting of local value for port ID
This was broken in a403df6662cd506fbf7fee8a46ebbf209142ffd7 (0.9.1). Add
some integration tests to ensure it works as expected.

Fix #183.

src/daemon/client.c
@@ -327,11 +327,13 @@ _client_handle_set_port(struct lldpd *cfg,
 		port->p_id = strdup(set->local_id);
 		port->p_id_len = strlen(set->local_id);
 		port->p_id_subtype = LLDP_PORTID_SUBTYPE_LOCAL;
+		port->p_descr_force = 0;
 	}
 	if (set->local_descr) {
 		log_debug("rpc", "requested change to Port Description");
 		free(port->p_descr);
 		port->p_descr = strdup(set->local_descr);
+		port->p_descr_force = 1;
 	}
 	switch (set->rxtx) {
 	case LLDPD_RXTX_TXONLY:

src/daemon/interfaces.c
@@ -517,8 +517,8 @@ interfaces_helper_port_name_desc(struct lldpd *cfg,
 	int portid_type = cfg->g_config.c_lldp_portid_type;
 	if (portid_type == LLDP_PORTID_SUBTYPE_IFNAME ||
 	    (portid_type == LLDP_PORTID_SUBTYPE_UNKNOWN && has_alias) ||
-	    (portid_type == LLDP_PORTID_SUBTYPE_LOCAL && has_alias)) {
-		if (portid_type != LLDP_PORTID_SUBTYPE_LOCAL) {
+	    (port->p_id_subtype == LLDP_PORTID_SUBTYPE_LOCAL && has_alias)) {
+		if (port->p_id_subtype != LLDP_PORTID_SUBTYPE_LOCAL) {
 			log_debug("interfaces", "use ifname for %s",
 			    hardware->h_ifname);
 			port->p_id_subtype = LLDP_PORTID_SUBTYPE_IFNAME;
@@ -529,19 +529,21 @@ interfaces_helper_port_name_desc(struct lldpd *cfg,
 			memcpy(port->p_id, hardware->h_ifname, port->p_id_len);
 		}
 
-		/* use the actual alias in the port description */
-		log_debug("interfaces", "using alias in description for %s",
-			  hardware->h_ifname);
-		free(port->p_descr);
-		if (has_alias) {
-			port->p_descr = strdup(iface->alias);
-		} else {
-			/* We don't have anything else to put here and for CDP
-			 * with need something non-NULL */
-			port->p_descr = strdup(hardware->h_ifname);
+		if (port->p_descr_force == 0) {
+			/* use the actual alias in the port description */
+			log_debug("interfaces", "using alias in description for %s",
+			    hardware->h_ifname);
+			free(port->p_descr);
+			if (has_alias) {
+				port->p_descr = strdup(iface->alias);
+			} else {
+				/* We don't have anything else to put here and for CDP
+				 * with need something non-NULL */
+				port->p_descr = strdup(hardware->h_ifname);
+			}
 		}
 	} else {
-		if (portid_type != LLDP_PORTID_SUBTYPE_LOCAL) {
+		if (port->p_id_subtype != LLDP_PORTID_SUBTYPE_LOCAL) {
 			log_debug("interfaces", "use MAC address for %s",
 			    hardware->h_ifname);
 			port->p_id_subtype = LLDP_PORTID_SUBTYPE_LLADDR;
@@ -552,11 +554,13 @@ interfaces_helper_port_name_desc(struct lldpd *cfg,
 			port->p_id_len = ETHER_ADDR_LEN;
 		}
 
-		/* use the ifname in the port description until alias is set */
-		log_debug("interfaces", "using ifname in description for %s",
-			  hardware->h_ifname);
-		free(port->p_descr);
-		port->p_descr = strdup(hardware->h_ifname);
+		if (port->p_descr_force == 0) {
+			/* use the ifname in the port description until alias is set */
+			log_debug("interfaces", "using ifname in description for %s",
+			    hardware->h_ifname);
+			free(port->p_descr);
+			port->p_descr = strdup(hardware->h_ifname);
+		}
 	}
 }
 

src/lldpd-structs.h
@@ -259,6 +259,7 @@ struct lldpd_port {
 	char			*p_id;
 	int			 p_id_len;
 	char			*p_descr;
+	int			 p_descr_force; /* Description has been forced by user */
 	u_int16_t		 p_mfs;
 
 #ifdef ENABLE_DOT3

tests/integration/test_basic.py
@@ -88,3 +88,77 @@ def test_management_address(lldpd1, lldpd, lldpcli, links, namespaces):
     with namespaces(1):
         out = lldpcli("-f", "keyvalue", "show", "neighbors")
         assert out["lldp.eth0.chassis.mgmt-ip"] == "172.25.21.47"
+
+
+def test_portid_subtype_ifname(lldpd1, lldpd, lldpcli, namespaces):
+    with namespaces(2):
+        lldpd()
+        result = lldpcli("configure", "lldp", "portidsubtype", "ifname")
+        time.sleep(3)
+    with namespaces(1):
+        out = lldpcli("-f", "keyvalue", "show", "neighbors")
+        assert out["lldp.eth0.port.ifname"] == "eth1"
+        assert out["lldp.eth0.port.descr"] == "eth1"
+
+
+def test_portid_subtype_with_alias(lldpd1, lldpd, lldpcli, links, namespaces):
+    with namespaces(2):
+        ipr = pyroute2.IPRoute()
+        idx = ipr.link_lookup(ifname="eth1")[0]
+        ipr.link('set', index=idx, ifalias="alias of eth1")
+        lldpd()
+    with namespaces(1):
+        out = lldpcli("-f", "keyvalue", "show", "neighbors")
+        assert out["lldp.eth0.port.ifname"] == "eth1"
+        assert out["lldp.eth0.port.descr"] == "alias of eth1"
+
+
+def test_portid_subtype_macaddress(lldpd1, lldpd, lldpcli, links, namespaces):
+    with namespaces(2):
+        ipr = pyroute2.IPRoute()
+        idx = ipr.link_lookup(ifname="eth1")[0]
+        ipr.link('set', index=idx, ifalias="alias of eth1")
+        lldpd()
+        result = lldpcli("configure", "lldp", "portidsubtype", "macaddress")
+        time.sleep(3)
+    with namespaces(1):
+        out = lldpcli("-f", "keyvalue", "show", "neighbors")
+        assert out["lldp.eth0.port.mac"] == "00:00:00:00:00:02"
+        assert out["lldp.eth0.port.descr"] == "eth1"
+
+
+def test_portid_subtype_local(lldpd1, lldpd, lldpcli, namespaces):
+    with namespaces(2):
+        lldpd()
+        result = lldpcli("configure", "lldp", "portidsubtype", "local", "localname")
+        time.sleep(3)
+    with namespaces(1):
+        out = lldpcli("-f", "keyvalue", "show", "neighbors")
+        assert out["lldp.eth0.port.local"] == "localname"
+        assert out["lldp.eth0.port.descr"] == "eth1"
+
+
+def test_portid_subtype_local_with_description(lldpd1, lldpd, lldpcli, namespaces):
+    with namespaces(2):
+        lldpd()
+        result = lldpcli("configure", "lldp", "portidsubtype", "local", "localname", "description", "localdescription")
+        time.sleep(3)
+    with namespaces(1):
+        out = lldpcli("-f", "keyvalue", "show", "neighbors")
+        assert out["lldp.eth0.port.local"] == "localname"
+        assert out["lldp.eth0.port.descr"] == "localdescription"
+
+
+def test_portid_subtype_local_with_alias(lldpd1, lldpd, lldpcli, namespaces):
+    with namespaces(2):
+        ipr = pyroute2.IPRoute()
+        idx = ipr.link_lookup(ifname="eth1")[0]
+        ipr.link('set', index=idx, ifalias="alias of eth1")
+        lldpd()
+        lldpd()
+        result = lldpcli("configure", "lldp", "portidsubtype", "local", "localname")
+        time.sleep(3)
+    with namespaces(1):
+        out = lldpcli("-f", "keyvalue", "show", "neighbors")
+        assert out["lldp.eth0.port.local"] == "localname"
+        assert out["lldp.eth0.port.descr"] == "alias of eth1"