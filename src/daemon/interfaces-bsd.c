bdc8f796579f0576b62166ed71c761510d9a2499
Vincent Bernat <vincent@bernat.im>
2016-06-19 08:43:06 +0200

    build: ensure typeof is correctly defined
    This could be typeof/__typeof/__typeof__.
    
    Fix #186.

--- src/daemon/interfaces-bsd.c
+++ src/daemon/interfaces-bsd.c
@@ -470,7 +470,11 @@ ifbsd_macphy(struct lldpd *cfg,
 {
 #ifdef ENABLE_DOT3
 	struct ifmediareq ifmr = {};
+#ifdef HAVE_TYPEOF
 	typeof(ifmr.ifm_ulist[0]) media_list[32] = {};
+#else
+	int media_list[32] = {};
+#endif
 	ifmr.ifm_ulist = media_list;
 	ifmr.ifm_count = 32;
 	struct lldpd_port *port = &hardware->h_lport;
