657d89df25f4db1db2e4d5783127b200dc8f0d1e
Vincent Bernat <vincent@bernat.ch>
2020-02-02 23:09:26 +0100

    osx: update recipe for HomeBrew

--- osx/lldpd.rb
+++ osx/lldpd.rb
@@ -1,8 +1,8 @@
 class Lldpd < Formula
   desc "Implementation of IEEE 802.1ab (LLDP)"
   homepage "https://vincentbernat.github.io/lldpd/"
-  url "https://media.luffy.cx/files/lldpd/lldpd-1.0.4.tar.gz"
-  sha256 "5319bc032fabf1008d5d91e280276aa7f1bbfbb70129d8526cd4526d7c22724f"
+  url "https://media.luffy.cx/files/lldpd/lldpd-1.0.5.tar.gz"
+  sha256 "2dd3b212f4dbabfcbb2794c0010b245f9f8e74b387984e757be6243a74c6cb99"
 
   option "with-snmp", "Build SNMP subagent support"
 
