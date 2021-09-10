f28fc79d7816a33af669d141e4234f592023c140
Vincent Bernat <vincent@bernat.ch>
2021-09-10 08:35:40 +0200

    osx: replace plist by service in brew recipe

--- osx/lldpd.rb
+++ osx/lldpd.rb
@@ -81,26 +81,8 @@ class Lldpd < Formula
   end
 
   plist_options startup: true
-
-  def plist
-    additional_args = ""
-    additional_args += "<string>-x</string>" if build.with? "snmp"
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_sbin}/lldpd</string>
-          #{additional_args}
-        </array>
-        <key>RunAtLoad</key><true/>
-        <key>KeepAlive</key><true/>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run build.with?("snmp") ? [opt_sbin/"lldpd", "-x"] : opt_sbin/"lldpd"
+    keep_alive true
   end
 end
