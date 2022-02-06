936fe92762dc18c7dd03627745a2911bfa3d1926
Vincent Bernat <vincent@bernat.ch>
2022-02-06 10:14:45 +0100

    build: explain current alternative

--- flake.nix
+++ flake.nix
@@ -12,6 +12,11 @@
         defaultPackage = pkgs.stdenv.mkDerivation rec {
           name = "lldpd";
           # We should be able to just use ./., but we have libevent as a submodule.
+          # Currently, an alternative would be to use:
+          #  nix build "git+file://$(pwd)?submodules=1"
+          # See:
+          # - https://github.com/NixOS/nix/pull/5434
+          # - https://github.com/NixOS/nix/pull/5497
           src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
           configureFlags = [
             "--localstatedir=/var"
