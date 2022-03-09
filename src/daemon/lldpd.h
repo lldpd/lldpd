78b1fbe5851078be149f68936b4ab4ebe7ac55f8
Luiz Amaral <email@luiz.eng.br>
2022-03-09 14:27:31 +0100

    freebsd: Use original if_ether.h

--- src/daemon/lldpd.h
+++ src/daemon/lldpd.h
@@ -33,8 +33,9 @@
 #include <string.h>
 #include <sys/queue.h>
 #include <sys/types.h>
-#include <netinet/if_ether.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/if_ether.h>
 #include <sys/un.h>
 
 #include "lldp-tlv.h"
