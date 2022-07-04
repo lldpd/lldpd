lldpd/lldpd

commit 8e9dbbf809186e6f13fb4a49333084f9fd6918d1

lldp: don't send VLANs when there are too many of them

src/daemon/protocols/lldp.c
@@ -59,7 +59,8 @@ static int _lldp_send(struct lldpd *global,
     u_int8_t p_id_subtype,
     char *p_id,
     int p_id_len,
-    int shutdown)
+    int shutdown,
+    int without_vlans)
 {
 	struct lldpd_port *port;
 	struct lldpd_chassis *chassis;
@@ -68,6 +69,7 @@ static int _lldp_send(struct lldpd *global,
 	u_int8_t *packet, *pos, *tlv;
 	struct lldpd_mgmt *mgmt;
 	int proto;
+	int vlans = 0;
 
 	u_int8_t mcastaddr_regular[] = LLDP_ADDR_NEAREST_BRIDGE;
 	u_int8_t mcastaddr_nontpmr[] = LLDP_ADDR_NEAREST_NONTPMR_BRIDGE;
@@ -248,6 +250,7 @@ static int _lldp_send(struct lldpd *global,
 	}
 	/* VLANs */
 	TAILQ_FOREACH(vlan, &port->p_vlans, v_entries) {
+		vlans++;
 		if (!(
 		      POKE_START_LLDP_TLV(LLDP_TLV_ORG) &&
 		      POKE_BYTES(dot1, sizeof(dot1)) &&
@@ -532,8 +535,21 @@ end:
 	return 0;
 
 toobig:
-	log_info("lldp", "Cannot send LLDP packet for %s, too big message", hardware->h_ifname);
 	free(packet);
+	if (vlans > 0 && !without_vlans) {
+		/* Retry without VLANs */
+		return _lldp_send(global,
+		    hardware,
+		    c_id_subtype,
+		    c_id,
+		    c_id_len,
+		    p_id_subtype,
+		    p_id,
+		    p_id_len,
+		    shutdown,
+		    1);
+	}
+	log_info("lldp", "Cannot send LLDP packet for %s, too big message", hardware->h_ifname);
 	return E2BIG;
 }
 
@@ -552,7 +568,7 @@ lldp_send_shutdown(struct lldpd *global,
 	    hardware->h_lport_previous_id_subtype,
 	    hardware->h_lport_previous_id,
 	    hardware->h_lport_previous_id_len,
-	    1);
+	    1, 0);
 }
 
 int
@@ -590,7 +606,7 @@ lldp_send(struct lldpd *global,
 		    port->p_id_subtype,
 		    port->p_id,
 		    port->p_id_len,
-		    0)) != 0)
+	            0, 0)) != 0)
 		return ret;
 
 	/* Record current chassis and port ID */

tests/integration/test_dot1.py
@@ -27,4 +27,13 @@ class TestLldpDot1(object):
             assert out['lldp.eth0.vlan.vlan-id'] == \
                 ['100', '200', '300', '4000']
 
+    def test_too_many_vlans(self, lldpd1, lldpd, lldpcli, namespaces, links):
+        with namespaces(2):
+            for v in range(100, 1000):
+                links.vlan('vlan{}'.format(v), v, 'eth1')
+            lldpd()
+        with namespaces(1):
+            out = lldpcli("-f", "keyvalue", "show", "neighbors", "details")
+            assert 'lldp.eth0.vlan' not in out
+
     # TODO: PI and PPVID (but lldpd doesn't know how to generate them)

commit a4ed10362c763331c7f2a1d75619d0a0deebb8da

lldp: don't send vlan when without_vlans is set
Fixes: 8e9dbbf80918 "lldp: don't send VLANs when there are too many of them

On interface with a lot of vlans, we still have "Too big packet"
notification on version 1.0.14

It looks like retry does exactly the same packet than first try

src/daemon/protocols/lldp.c
@@ -249,17 +249,19 @@ static int _lldp_send(struct lldpd *global,
 		}
 	}
 	/* VLANs */
-	TAILQ_FOREACH(vlan, &port->p_vlans, v_entries) {
-		vlans++;
-		if (!(
-		      POKE_START_LLDP_TLV(LLDP_TLV_ORG) &&
-		      POKE_BYTES(dot1, sizeof(dot1)) &&
-		      POKE_UINT8(LLDP_TLV_DOT1_VLANNAME) &&
-		      POKE_UINT16(vlan->v_vid) &&
-		      POKE_UINT8(strlen(vlan->v_name)) &&
-		      POKE_BYTES(vlan->v_name, strlen(vlan->v_name)) &&
-		      POKE_END_LLDP_TLV))
-			goto toobig;
+	if (!without_vlans) {
+		TAILQ_FOREACH(vlan, &port->p_vlans, v_entries) {
+			vlans++;
+			if (!(
+			      POKE_START_LLDP_TLV(LLDP_TLV_ORG) &&
+			      POKE_BYTES(dot1, sizeof(dot1)) &&
+			      POKE_UINT8(LLDP_TLV_DOT1_VLANNAME) &&
+			      POKE_UINT16(vlan->v_vid) &&
+			      POKE_UINT8(strlen(vlan->v_name)) &&
+			      POKE_BYTES(vlan->v_name, strlen(vlan->v_name)) &&
+			      POKE_END_LLDP_TLV))
+				goto toobig;
+		}
 	}
 	/* Protocol Identities */
 	TAILQ_FOREACH(pi, &port->p_pids, p_entries) {