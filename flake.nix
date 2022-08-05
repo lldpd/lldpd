31c49c1593bf2ca436fd96cdd7b71694551b48a3
Vincent Bernat <vincent@bernat.ch>
2022-08-05 12:38:29 +0200

    build: update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, flake-utils, ... }@inputs:
