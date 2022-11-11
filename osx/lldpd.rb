62364b80bd8e99f16b96abb943c0ca84cdff8b24
Vincent Bernat <vincent@bernat.ch>
2022-11-11 17:43:53 +0100

    osx: update Homebrew recipe

--- osx/lldpd.rb
+++ osx/lldpd.rb
@@ -1,8 +1,8 @@
 class Lldpd < Formula
   desc "Implementation of IEEE 802.1ab (LLDP)"
   homepage "https://lldpd.github.io/"
-  url "https://media.luffy.cx/files/lldpd/lldpd-1.0.13.tar.gz"
-  sha256 "d639827fd8a27720d1bfd94bc52eca24af63ddcc3c9d2da60788778889d84701"
+  url "https://media.luffy.cx/files/lldpd/lldpd-1.0.16.tar.gz"
+  sha256 "e3b391650c7ba67cea2fe84d67fdb4d7fc8aa1ec5cf86eb8bb984711df8465a9"
   license "ISC"
 
   livecheck do
