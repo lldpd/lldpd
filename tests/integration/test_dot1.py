379b8ab111c760a0c92550a1ad0c37c5ab225374
Vincent Bernat <vincent@bernat.ch>
2022-12-09 09:26:48 +0100

    tests: mark "too_many_vlans" test as unreliable

--- tests/integration/test_dot1.py
+++ tests/integration/test_dot1.py
@@ -28,11 +28,12 @@ class TestLldpDot1(object):
             ]
             assert out["lldp.eth0.vlan.vlan-id"] == ["100", "200", "300", "4000"]
 
+    @pytest.mark.skip(reason="unreliable test")
     def test_too_many_vlans(self, lldpd1, lldpd, lldpcli, namespaces, links):
         with namespaces(2):
             for v in range(100, 1000):
                 links.vlan("vlan{}".format(v), v, "eth1")
-            lldpd(sleep=6)
+            lldpd()
         with namespaces(1):
             out = lldpcli("-f", "keyvalue", "show", "neighbors", "details")
             assert "lldp.eth0.vlan" not in out
