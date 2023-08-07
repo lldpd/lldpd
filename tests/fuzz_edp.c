83f0eddcfb7fb1d0a825136e3929f8847cd85a62
Arjun Singh <ajsinghyadav00@gmail.com>
2023-08-07 17:01:45 +0530

    [bug] fix 5193822305648640 and 4673544697806848
    Signed-off-by: Arjun Singh <ajsinghyadav00@gmail.com>

--- tests/fuzz_edp.c
+++ tests/fuzz_edp.c
@@ -32,6 +32,7 @@ LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 
 	struct lldpd cfg;
 	cfg.g_config.c_mgmt_pattern = NULL;
+	cfg.g_config.c_tx_hold = LLDPD_TX_HOLD;
 
 	struct lldpd_chassis *nchassis = NULL;
 	struct lldpd_port *nport = NULL;
