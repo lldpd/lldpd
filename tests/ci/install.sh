18dc7145d497bb6d1f318b2221b21553d5f88ca1
Vincent Bernat <vincent@bernat.ch>
2023-12-01 22:53:02 +0100

    build: update automake for OpenBSD

--- tests/ci/install.sh
+++ tests/ci/install.sh
@@ -5,7 +5,7 @@ set -e
 case "$(uname -s)" in
     OpenBSD)
         sudo pkg_add -I \
-             automake-1.16.3 autoconf-2.71 libtool \
+             automake-1.16.5 autoconf-2.71 libtool \
              libevent libxml check \
              git
         ;;
