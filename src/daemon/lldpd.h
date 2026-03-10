6d101f0626560bffdc21e3528a8a0ad1fd2e21aa
Vincent Bernat <vincent@bernat.ch>
2026-03-10 08:34:22 +0100

    daemon: only keep one privileged op to remove both socket and lock

--- src/daemon/lldpd.h
+++ src/daemon/lldpd.h
@@ -199,7 +199,6 @@ void priv_init(const char *);
 #endif
 void priv_wait(void);
 void priv_ctl_cleanup(void);
-void priv_ctl_cleanup_lock(void);
 char *priv_gethostname(void);
 #ifdef HOST_OS_LINUX
 int priv_open(const char *);
@@ -217,7 +216,6 @@ int priv_snmp_socket(struct sockaddr_un *);
 enum priv_cmd {
 	PRIV_PING,
 	PRIV_DELETE_CTL_SOCKET,
-	PRIV_DELETE_CTL_SOCKET_LOCK,
 	PRIV_GET_HOSTNAME,
 	PRIV_OPEN,
 	PRIV_IFACE_INIT,
