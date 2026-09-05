789c2d31637926524443ed6e74526ec544508500
Vincent Bernat <vincent@bernat.ch>
2026-09-05 08:21:47 +0200

    build: switch from flake-utils to forAllSystems/perSystem idiom

--- flake.nix
+++ flake.nix
@@ -1,56 +1,44 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = inputs.nixpkgs.legacyPackages."${system}";
-      in
-      {
-        packages.default = pkgs.stdenv.mkDerivation rec {
-          name = "lldpd";
-          # We should be able to just use ./., but we have libevent as a submodule.
-          # Currently, we should use:
-          #  nix build ".?submodules=1"
-          # See:
-          # - https://github.com/NixOS/nix/pull/5434
-          # - https://github.com/NixOS/nix/pull/5497
-          src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-          configureFlags = [
-            "--localstatedir=/var"
-            "--enable-pie"
-            "--with-snmp"
-            "--with-systemdsystemunitdir=\${out}/lib/systemd/system"
-          ];
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      perSystem = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages."${system}";
+          llvm = pkgs.llvmPackages_14;
+          clang-tools = pkgs.clang-tools.override { llvmPackages = llvm; };
+          lldpd = pkgs.stdenv.mkDerivation rec {
+            name = "lldpd";
+            # We should be able to just use ./., but we have libevent as a submodule.
+            # Currently, we should use:
+            #  nix build ".?submodules=1"
+            # See:
+            # - https://github.com/NixOS/nix/pull/5434
+            # - https://github.com/NixOS/nix/pull/5497
+            src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+            configureFlags = [
+              "--localstatedir=/var"
+              "--enable-pie"
+              "--with-snmp"
+              "--with-systemdsystemunitdir=\${out}/lib/systemd/system"
+            ];
 
-          nativeBuildInputs = with pkgs; [ pkg-config autoreconfHook git check ];
-          buildInputs = with pkgs; [ libevent readline net-snmp openssl ];
-          outputs = [ "out" "dev" "man" "doc" ];
-        };
-        apps = {
-          # Use:
-          #  nix run ".?submodules=1#lldpcli" -- --help
-          lldpd = {
-            type = "app";
-            program = "${self.packages."${system}".default}/bin/lldpd";
+            nativeBuildInputs = with pkgs; [ pkg-config autoreconfHook git check ];
+            buildInputs = with pkgs; [ libevent readline net-snmp openssl ];
+            outputs = [ "out" "dev" "man" "doc" ];
           };
-          lldpcli = {
-            type = "app";
-            program = "${self.packages."${system}".default}/bin/lldpcli";
-          };
-        };
-        devShells.default =
-          let
-            llvm = pkgs.llvmPackages_14;
-            clang-tools = pkgs.clang-tools.override { llvmPackages = llvm; };
-          in
-          pkgs.mkShell {
+        in
+        {
+          package = lldpd;
+          devShell = pkgs.mkShell {
             name = "lldpd-dev";
             buildInputs =
-              self.packages."${system}".default.nativeBuildInputs ++
-              self.packages."${system}".default.buildInputs ++ [
+              lldpd.nativeBuildInputs ++
+              lldpd.buildInputs ++ [
                 clang-tools # clang-format (C)
                 llvm.libclang.python # git-clang-format (C)
                 pkgs.python3Packages.black # black (Python)
@@ -75,5 +63,26 @@
                 '')
               ];
           };
+        });
+    in
+    {
+      packages = forAllSystems (system: {
+        default = perSystem."${system}".package;
       });
+      apps = forAllSystems (system: {
+        # Use:
+        #  nix run ".?submodules=1#lldpcli" -- --help
+        lldpd = {
+          type = "app";
+          program = "${perSystem."${system}".package}/bin/lldpd";
+        };
+        lldpcli = {
+          type = "app";
+          program = "${perSystem."${system}".package}/bin/lldpcli";
+        };
+      });
+      devShells = forAllSystems (system: {
+        default = perSystem."${system}".devShell;
+      });
+    };
 }
